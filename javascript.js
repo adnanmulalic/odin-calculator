a3908b91d95c0e95d188927508dc0d3fe0c4380c
adnanmulalic <adnan.mulalic3@gmail.com>
2022-09-08 11:27:46 +0200

    wip

--- javascript.js
+++ javascript.js
@@ -11,54 +11,6 @@ function divide(a, b) {
     return a / b;
 }
 
-let number1 = null;
-let number2 = null;
-let currentOperator = null;
-
-let displayValue = document.querySelector("#display-value");
-const equalsBtn = document.querySelector("#equals");
-const buttons = document.querySelector("#buttons");
-const numbers = document.querySelectorAll(".numbers");
-const operators = document.querySelectorAll(".operators");
-let result = 0;
-
-equalsBtn.addEventListener("click", operate(currentOperator, number1, number2), (e) => {
-
-    e.stopPropagation;
-})
-
-numbers.forEach(number => {
-    number.addEventListener("click", () => {
-        if (number.id === "clear") {
-            displayValue.innerHTML = 0;
-            number1 = null;
-            number2 = null;
-            currentOperator = null;
-        } else if (number1 === null || number2 === null) {
-            number1 = number.innerHTML;
-            displayValue.innerHTML = number1;
-        }
-    })
-});
-
-let currentNumber1 = number1;
-
-operators.forEach(operator => {
-    operator.addEventListener("click", () => {
-        currentOperator = operator.innerHTML;
-    })
-    numbers.forEach(number => {
-        number.addEventListener("click", () => {
-            if (number.id === "clear") {
-                displayValue.innerHTML = 0;
-            } else {
-                number2 = number.innerHTML;
-            }
-        })
-    })
-});
-
-
 function operate(operator, num1, num2) {
     if (operator === "+") {
         return add(num1, num2);
@@ -73,3 +25,49 @@ function operate(operator, num1, num2) {
         return divide(num1, num2);
     }
 };
+
+let number1 = "";
+let number2 = "";
+let currentNumber = "";
+let currentOperator = null;
+
+let displayValue = document.querySelector("#display-value");
+const equalsBtn = document.querySelector("#equals");
+const buttons = document.querySelectorAll("#buttons");
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators:not(#equals)");
+let result = 0;
+
+
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        currentOperator = operator.innerHTML;
+        number1 = currentNumber;
+        currentNumber = "";
+    })
+})
+
+equalsBtn.addEventListener("click", () => {
+    number2 = currentNumber;
+    displayValue = operate(currentOperator, number1, number2);
+})
+
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if (number.id === "clear") {
+            displayValue.innerHTML = 0;
+            currentNumber = "";
+            number1 = "";
+            number2 = "";
+            currentOperator = null;
+        }
+        else if (number1 != "") {
+            currentNumber += number.innerHTML
+            displayValue.innerHTML = currentNumber;
+        }
+        else {
+        currentNumber += number.innerHTML; 
+        displayValue.innerHTML = currentNumber;
+        }
+    })
+});
