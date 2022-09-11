60234e9c88ca455d243793786f56944640037a1b
adnanmulalic <adnan.mulalic3@gmail.com>
2022-09-11 11:50:53 +0200

    Calculations working properly. Need to limit result to 2 decimals, limits numbers in display, add error messages

--- javascript.js
+++ javascript.js
@@ -36,7 +36,7 @@ const equalsBtn = document.querySelector("#equals");
 const buttons = document.querySelectorAll("#buttons");
 const numbers = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operators:not(#equals)"); //(".operators:not(#equals)")
-let result = false;
+let result = "";
 
 
 operators.forEach(operator => {
@@ -47,7 +47,12 @@ operators.forEach(operator => {
             number1 = displayValue.innerHTML;
             currentOperator = operator.innerHTML;
             currentNumber = "";
-        } else {
+        } else if (number1 != "" && currentNumber === "") {
+            currentOperator = operator.innerHTML;
+            currentNumber = number1;
+            number2 = "";
+        }
+         else {
             currentOperator = operator.innerHTML;
             number1 = currentNumber;
             currentNumber = "";
@@ -58,6 +63,9 @@ operators.forEach(operator => {
  equalsBtn.addEventListener("click", () => {
     number2 = currentNumber;
     currentNumber = displayValue.innerHTML = operate(currentOperator, parseInt(number1), parseInt(number2));
+    //number1 = displayValue.innerHTML;
+    number1 = "";
+    //currentNumber = "";
     
 })
 
@@ -74,7 +82,11 @@ numbers.forEach(number => {
             currentNumber += number.innerHTML
             displayValue.innerHTML = currentNumber;
         }
-        
+        else if (number2 === "" && currentOperator != null) {
+            number1 = currentNumber;
+            currentNumber = number.innerHTML;
+            displayValue.innerHTML = currentNumber;
+        }
         else {
         currentNumber += number.innerHTML; 
         displayValue.innerHTML = currentNumber;
